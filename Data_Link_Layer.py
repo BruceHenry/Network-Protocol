c8991cf6a4b103ba6df44ace0da5ec2d1d286385
BaoHan HP <hbao@wpi.edu>
2016-11-01 16:33:34 -0400

    gbn modified 1

--- Data_Link_Layer.py
+++ Data_Link_Layer.py
@@ -8,9 +8,6 @@ class packet:
     ack = 0
     buffer = ""
 
-    def __init__(self):
-        pass
-
     def __init__(self, buffer):
         self.buffer = buffer
 
@@ -19,30 +16,23 @@ class packet:
         self.ack = ack
 
 
-
-# packet format(temp):
-# 1st byte:seq
-# 2nd byte:space
-# 3rd byte:ack
-# 4th byte:space
-# 5th and later:data
-
 class dataLinkLayer:
     p = physicalLayer()
     windowSize = 5
     send_buffer = []
     receive_buffer = []
 
-    next_expected_seq=0
-    timer=self.timer();
+    next_expected_seq = 0
 
     base = 0
     next_seq = 0
 
-    def send(self, mode, buffer):
+    def __init__(self):
+        t = self.timer(3)
 
+    def send(self, mode, buffer):
         if mode == 1:
-            self.go_back_n_sender(packet(buffer))
+            self.go_back_n_send(packet(buffer))
             return 1
         elif mode == 2:
             self.selective_repeat_sender()
@@ -51,105 +41,85 @@ class dataLinkLayer:
             return -1
 
     def receive(self, mode):
-        if mode==1:
+        if mode == 1:
             self.go_back_n_receiver()
             return 1
-        elif mode==2:
+        elif mode == 2:
             self.selective_repeat_receiver()
             return 1
         else:
             return -1
 
-    # def send_ack(self, seq):
-    #     self.seq += 1
-    #     ack = seq + 1
-    #     message = str(self.seq) + " " + str(ack)
-    #     return message
-
     def make_packet(self, pkt):
-        packet_without_checksum=str(pkt.seq)+ str(pkt.ack)+pkt.buffer
-        checksum=self.ichecksum(packet_without_checksum)
-        return str(pkt.seq) + " " + str(pkt.ack) + " " +str(checksum)+" "+pkt.buffer
+        packet_without_checksum = str(pkt.seq) + str(pkt.ack) + pkt.buffer
+        checksum = self.ichecksum(packet_without_checksum)
+        return str(pkt.seq) + " " + str(pkt.ack) + " " + str(checksum) + " " + pkt.buffer
 
-    def refuse(self,packet):
+    def refuse(self, packet):
         print("Window full, cannot send data now")
         return False
 
+    def go_back_n_send(self, packet):
+        # send
+        if self.next_seq < self.base + self.windowSize:
+            self.send_buffer.append(packet)
+            self.send_buffer[self.next_seq].set_seq_ack(self.next_seq, 0)
+            self.p.send(self.make_packet(self.send_buffer[self.next_seq]))
+            if self.base == self.next_seq:
+                self.setTimer()
+                self.t.start()
+            self.next_seq += 1
+            return True
+        else:
+            self.refuse(packet)
 
-
-    # thread for sending
-    def go_back_n_send(self,packet):
-            # send
-            if self.next_seq < self.base + self.windowSize:
-                self.send_buffer.append(packet)
-                self.send_buffer[self.next_seq].set_seq_ack(self.next_seq,0)
-                self.p.send(self.make_packet(self.send_buffer[self.next_seq]))
-                if self.base == self.next_seq:
-                    self.timer.start()
-                self.next_seq += 1
-                return True
-            else:
-                self.refuse(packet)
-
-
-    # thread for timer
     def go_back_n_timeout(self):
-            # Timeout
-            self.timer.start()
-            for i in range(self.base, self.next_seq):
-                self.p.send(self.make_packet(self.send_buffer[i]))
-
-    # thread for receiving ack...
-    def go_back_n_receive(self):
-        while True:
-            buffer = self.p.receive()
-            if buffer[4] == '1':  # check integrity(suppose 1 means NOT intact)
-                self.p.send(self.make_packet(self.send_buffer[buffer[2]]))
-            else:
-                self.base = buffer[2] + 1
-                if self.base == self.next_seq:
-                    self.timer.cancel()
-                else:
-                    self.timer.start()
-
+        # Timeout
+        self.t.cancel()
+        self.setTimer()
+        self.t.start()
+        for i in range(self.base, self.next_seq):
+            self.p.send(self.make_packet(self.send_buffer[i]))
 
     # the receiver in go_back_n
     def go_back_n_receiver(self):
-
-
         buffer = self.p.receive()
-        packet=buffer.split(" ")
-        seq=packet[0]
-        ack=int(packet[1])
-        checksum=packet[2]
-        if(not ack):
-            data=packet[3]
-            valid=self.ichecksum(seq+ack+data,checksum)
+        packet = buffer.split(" ")
+        seq = packet[0]
+        ack = int(packet[1])
+        checksum = packet[2]
+        if not ack:
+            data = packet[3]
+            valid = self.ichecksum(seq + ack + data, checksum)
             ack_pkt = packet("")
-            if(not valid and self.next_expected_seq==seq):
-                ack_pkt.set_seq_ack(self.next_expected_seq,1)
+            if (not valid and self.next_expected_seq == seq):
+                ack_pkt.set_seq_ack(self.next_expected_seq, 1)
                 self.p.send(self.make_packet(ack_pkt))
-                self.next_expected_seq+=1
-                return  data
-            elif(self.next_expected_seq!=seq):
+                self.next_expected_seq += 1
+                return data
+            elif self.next_expected_seq != seq:
                 print("Out of order packet")
-                ack_pkt.set_seq_ack(self.next_expected_seq-1, 1)
+                ack_pkt.set_seq_ack(self.next_expected_seq - 1, 1)
                 self.p.send(self.make_packet(ack_pkt))
                 return False
         else:
-            valid = self.ichecksum(seq + ack , checksum)
-            if(not valid and self.base!=int(seq)):
-                self.base=int(seq)+1
-                if(self.base==self.next_seq)
-                    self.timer.cancel()
-
-
+            valid = self.ichecksum(seq + ack, checksum)
+            if (not valid and self.base == int(seq)):
+                self.base = int(seq) + 1
+                if self.base == self.next_seq:
+                    self.t.cancel()
+                else:
+                    self.setTimer()
+                    self.t.start()
 
     # Return a timer object
     # Use timer.cancel() to stop the timer
     def timer(self, timeout):
-        timer = threading.Timer(timeout, self.go_back_n_timeout())
+        timer = threading.Timer(timeout, self.go_back_n_timeout, ())
         return timer
+
+    def setTimer(self):
+        self.t = self.timer(3)
 
     def ichecksum(data, sum=0):
         """ Compute the Internet Checksum of the supplied data.  The checksum is
